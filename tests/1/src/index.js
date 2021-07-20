098ec118ca3e380c1fa542df0b9e0fd6ba7168db
Daniel Kurowski <daniel.kurowski@grifart.cz>
2021-07-20 16:24:06 +0200

    Added images to test case

--- tests/1/src/index.js
+++ tests/1/src/index.js
@@ -1,5 +1,9 @@
-
 import "./scss/something.scss";
+
+import "./images/test.jpg";
+import "./images/test.gif";
+import "./images/test.png";
+import "./images/test.svg";
 
 (function() {
 	console.log("something");
